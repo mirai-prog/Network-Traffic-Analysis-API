77715fbd01234a60b10aa564cd6dee4a3e894911
mirai-prog <pankkkou@icloud.com>
2024-12-16 11:00:34 +0100

    connected upload router

--- project/app/main.py
+++ project/app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from app.routers import upload
 
 app = FastAPI()
+
+@app.get("/healthcheck")
+def healthcheck():
+    """
+    Эндпоинт проверки работоспособности API.
+    """
+    return {"status": "ok"}
+
+
+app.include_router(upload.router, prefix="/files", tags=["File Operations"])
 
 @app.get("/healthcheck")
 def healthcheck():
