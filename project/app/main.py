3e80d5ddc44a86a0d4a12ce3a0a75ff96efe6c36
mirai-prog <pankkkou@icloud.com>
2024-12-16 11:14:48 +0100

    correct uploud.py

--- project/app/main.py
+++ project/app/main.py
@@ -3,14 +3,6 @@ from app.routers import upload
 
 app = FastAPI()
 
-@app.get("/healthcheck")
-def healthcheck():
-    """
-    Эндпоинт проверки работоспособности API.
-    """
-    return {"status": "ok"}
-
-
 app.include_router(upload.router, prefix="/files", tags=["File Operations"])
 
 @app.get("/healthcheck")
